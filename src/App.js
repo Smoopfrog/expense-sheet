f03d9147f59c999d72c90341c1d967d8bd1fae5b
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-05 12:41:24 -0700

    initialize git

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Stay Golden!</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
