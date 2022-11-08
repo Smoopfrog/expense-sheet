e6b95d983320fa1c6262f794b16147e04f5dffe0
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-08 12:32:54 -0800

    create expense form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,12 +7,12 @@ const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
   const clickHandled = () => {
-    setTitle('Updated!')
-  }
-  
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
