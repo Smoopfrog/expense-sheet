c6704d3ddedd2fa75f9fef92e9a03e35776c2c0c
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-09 18:36:24 -0800

    add conditional message if no expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,28 +6,37 @@ import Card from "../UI/Card";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
 
-  const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear)
-  
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
   const addYearHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    })
+  }
+
   return (
-    <Card className="expenses">   
+    <Card className="expenses">
       <ExpensesFilter
         selectedYear={filteredYear}
         onYearChange={addYearHandler}
       />
-      {filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
+      {expensesContent}
+
     </Card>
   );
 };
