736d89c6dbad38a2beb6e98a020f143b7d497730
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-09 22:38:54 -0800

    add toggle button for NewExpense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,33 @@
-import './NewExpense.css'
-import ExpenseForm from './ExpenseForm';
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
-  const saveExpenseDateHandler = enteredExpenseData => {
+  const [showExpenseForm, setShowExpenseForm] = useState(false);
+  const saveExpenseDateHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
-    }
+      id: Math.random().toString(),
+    };
     props.onAddExpense(expenseData);
-  }
+  };
+
+  const expenseFormHandler = () => {
+    setShowExpenseForm(!showExpenseForm);
+  };
 
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseDate={saveExpenseDateHandler} />
+    <div className="new-expense">
+      {showExpenseForm ? (
+        <ExpenseForm
+          onSaveExpenseDate={saveExpenseDateHandler}
+          onCancel={expenseFormHandler}
+        />
+      ) : (
+        <button onClick={expenseFormHandler}>Add New Expense</button>
+      )}
     </div>
-  )  
-}
+  );
+};
 
 export default NewExpense;
